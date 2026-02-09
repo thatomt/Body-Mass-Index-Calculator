f6a1bf53bed75973fa247c341a554c47a5938b0e
Thato M <thatomsekele@gmail.com>
2026-02-09 22:47:39 +0200

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -3,4 +3,5 @@ def calculate_bmi(weight, height):
   bmi = weight / (height ** 2)
   return bmi
 
-def analyze_bmi
+def analyze_bmi(bmi):     # BMI values < 18.5 UW, 18.5 - 24.9 N, > 25 OV, > 30 OB
+  
