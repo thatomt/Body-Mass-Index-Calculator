3e1c15dd9f511665a5028944012f23bfef6e2264
Thato M <thatomsekele@gmail.com>
2026-02-12 21:00:16 +0200

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -10,4 +10,5 @@ def analyze_bmi(bmi):     # BMI values < 18.5 UW, 18.5 - 24.9 N, > 25 OV, > 30 O
     return "Normal weight"
   elif bmi < 30:
     return "Overweight"
-    
+  else:
+    return "Obese" 
